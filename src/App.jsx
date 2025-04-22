5ff246fd195af40b0a04f475e28c8ac10d936ec4
Your name <Your email>
2025-04-22 12:32:18 +0200

    first card implementation

---
+++ src/App.jsx
@@ -0,0 +1,30 @@
+import "./App.css";
+
+const imageSrc ="https://www.lego.com/cdn/cs/set/assets/blt6cdf0b53146b5519/10294_Prod.png?format=webply&fit=bounds&quality=100&width=400&height=400&dpr=1";
+const title = "Titanic";
+const tag = "historique" ;
+const price = 199.00 ;
+const status = "disponible" ;
+const addBtn = "ajouter";
+
+
+function Card() {
+  return (
+    <div>
+      <img src={imageSrc} className="img" alt={`image of ${title}`} />
+      <h2 className="titre">{title}</h2>
+      <p className="tag">{tag}</p>
+      <p className="prix">{price} €</p>
+      <p className="status">{status}</p>
+      <p className="addBtn">{addBtn}</p>
+    </div>
+  );
+}
+
+function App() {
+  return (
+      <Card/>
+  );
+}
+
+export default App;
