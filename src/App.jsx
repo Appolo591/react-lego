fe1de9872405927604c5718db8cc1bc420f17a00
Your name <Your email>
2025-04-23 09:45:30 +0200

    ajout card2

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,15 @@
 import "./App.css";
-
-const imageSrc ="https://www.lego.com/cdn/cs/set/assets/blt6cdf0b53146b5519/10294_Prod.png?format=webply&fit=bounds&quality=100&width=400&height=400&dpr=1";
-const title = "Titanic";
-const tag = "historique" ;
-const price = 199.00 ;
-const status = "disponible" ;
-const addBtn = "ajouter";
-
-
-function Card() {
-  return (
-    <div>
-      <img src={imageSrc} className="img" alt={`image of ${title}`} />
-      <h2 className="titre">{title}</h2>
-      <p className="tag">{tag}</p>
-      <p className="prix">{price} €</p>
-      <p className="status">{status}</p>
-      <p className="addBtn">{addBtn}</p>
-    </div>
-  );
-}
+import Header from "./Components/Header/Header";
+import List from "./Components/List/List";
+import Footer from "./Components/Footer/Footer";
 
 function App() {
   return (
-      <Card/>
+    <>
+      <Header />
+      <List />
+      <Footer />
+    </>
   );
 }
 
